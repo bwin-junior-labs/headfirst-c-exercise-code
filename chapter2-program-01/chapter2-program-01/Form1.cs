f3692f7c35acf021a86af308aa5738fb8e933886
Geoff Bennett <geoff.bennett@bwinparty.com>
2012-10-21 19:33:18 +0200

    study code

---
+++ chapter2-program-01/chapter2-program-01/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter2_program_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Here's a great way to use the IDE to see how this code works!
+            //
+            //First create a new project in the IDE add a button. Next, double-click on the 
+            //button so the IDE adds a button1_Click method to your program. Fill in that 
+            //method with all of the code, starting with "int number = 15;".
+            //
+            //Now put a breakpoint on the first line by right-clicking on it and choosing
+            //"Breakpoint >> Insert Breakpoint". The line should turn red. 
+            //
+            //Next, start debugging your program. You'll see it break on the line where you 
+            //inserted the breakpoint. Your program's just paused! If you click the Run
+            //toolbar button (or hit F5), it will continue. Right-click on "number" and
+            //choose "Expression: 'number' >> Add Watch" from the menu. The bottom panel in
+            //the IDE should change to the Watches window. and there should be a line in that
+            //window for "number". Step through the program line by line using Step Over (F10).
+            //You can see the value of the "number" variable change as you go!
+            //
+            //Do the same for the count, result, yesNo, and anotherBool variables. 
+
+            int number = 15;
+            number = number + 10;
+            number = 36 * 15;
+            number = 12 - (42 / 7);
+            number += 10;
+            number *= 71 / 3;
+
+            int count = 0;
+            count++;
+            count--;
+
+            string result = "hello";
+            result += " again " + result;
+            MessageBox.Show(result);
+            result = "the value is: " + count;
+            result = "";
+
+            bool yesNo = false;
+            bool anotherBool = true;
+            yesNo = !anotherBool;
+
+            {
+                
+            }
+        }
+    }
+}
---
+++ chapter2-program-02/chapter2-program-02/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter2_program_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Loop #1 pg.71 
+            // the loop only runs once, and that makes sense because the while loop only runs
+            // while count is > 0. The final calculation times count by -1 which came to -15            
+            int count = 5;
+            int loopCount = 0; // this is my addition to count how many times the while loop is run
+            while (count > 0)
+            {
+                count = count * 3;
+                count = count * -1;
+                loopCount++; // as is this
+            }
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Loop #2 
+            // this loop runs forever because the i variable always equals 0 
+            int i = 0;
+            int count = 2;
+            int loopCount = 0;
+            while (i == 0) {
+                count = count * 3;
+                count = count * -1;
+                loopCount++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Loop #5 - loops 8 times
+            int p = 2;
+            int loopCount = 0;
+            for (int q = 2; q < 32;
+                q = q * 2)
+            {
+                while (p < q)
+                {
+                    p = p * 2;
+                }
+                q = p - q;
+                loopCount++;
+            }
+            
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Sharpen Your Pencil Pg. 72
+            // makes sense when you step through it
+            int result = 0; // this variable will hold the final result
+            int x = 6; // declare a variable x and set it to 6
+            int loopCount = 0;
+            int loopCount2 = 0;
+            while (x > 3) {
+            // execute these statements as long as x is greater than 3
+                result = result + x; // add x to the result variable
+                x = x - 1; // subtract 1 from the value of x 
+                loopCount++;
+            }
+            for (int z = 1; z < 3; z = z + 1) {
+                // start the loop by declaring a variable z and setting it to 1
+                // keep looping as long as z is less than 3
+                // after each loop, add 1 to z
+                result = result + z; // add the value of z to result
+                loopCount2++;
+            }
+            // The next statement will pop up a message box that says
+            // The result is 18
+            MessageBox.Show("The result is " + result);                         
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {   // loop #3
+            int j = 2;
+            int loopCount = 0;
+            int loopCount2 = 0; 
+            for (int i = 1; i < 100;
+                i = i * 2) // it appears that the first time the loop                       
+            {              // goes around, i is not * 2, i.e. that part
+                j = j - i; // of the statement is not run
+                while (j < 25)
+                {
+                    j = j + 5;
+                    loopCount++;
+                }
+                loopCount2++;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // loop #4
+            int loopCount = 0;
+            while (true) { int i = 1; loopCount++; }
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string Result = "";
+            int x = 3;
+            if (x == 1) {
+                Result = Result + "d";
+                x = x - 1;
+
+            while (x > 0)
+            {
+                x = x - 1;
+                Result = Result + "-";
+            }
+
+            MessageBox.Show(Result);
+        }
+    }
+}
